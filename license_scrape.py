e191109862f24b72ad850f34e6f4c96a54184d90
Andy Zoutte <andy@Andy-Zouttes-MacBook-Pro.local>
2016-10-17 16:35:02 -0600

    added scapes for the download URLS

--- license_scrape.py
+++ license_scrape.py
@@ -1,13 +1,26 @@
 
 import urllib, re, time, os
-homeDir="/Users/andy"
+homeDir = "/Users/andy"
+licFiles = homeDir + "/licenses"
+downloads = homeDir + "/downloads"
+
+def findDownload(stringMatch, URL):
+    sock = urllib.urlopen(URL)
+    htmlSource = sock.read()
+    sock.close()
+    arrayOfLines = htmlSource.split()
+    for line in arrayOfLines:
+        line =  str(line)
+        if re.match(stringMatch, line) is not None:
+            line = line[7:len(line)-9]
+            break
+    return line
+
 
 sock = urllib.urlopen('https://license.aspera.us/evals')
 htmlSource = sock.read()
 sock.close()
 arrayOfLines = htmlSource.split()
-#for line in arrayOfLines:
-#    print(line)
 n = arrayOfLines.index(b'<strong>Username</strong>') + 2
 passWord = arrayOfLines[n].decode("utf-8")
 passWord = re.sub('<[^<]+?>', '', passWord)
@@ -19,11 +32,21 @@ print(userName)
 
 month = time.strftime("%m")
 year = time.strftime("%Y")
-if not os.path.exists(homeDir + '/licenses'):
+if not os.path.exists(licFiles):
     print("it doesn't exist")
-    os.mkdir(homeDir + '/licenses', 0o700)
+    os.mkdir(licFiles, 0o700)
 urllib.urlretrieve("https://license.aspera.us/evals/" + year + "/" + month + ".zip", homeDir + "/licenses/licenses.zip")
 unzipCommand = 'unzip -o ' + homeDir + '/licenses/licenses.zip -d ' + homeDir + '/licenses'
 os.system(unzipCommand)
+
+EntSrvDownload = findDownload('(.+?)/download/sw/entsrv(.+?)-linux-64.rpm(.+?)', 'http://downloads.asperasoft.com/en/downloads/1')
+SharesDownload = findDownload('(.+?)/download/sw/shares/(.+?).x86_64.rpm(.+?)', 'http://downloads.asperasoft.com/en/downloads/34')
+FaspexDownload = findDownload('(.+?)/download/sw/faspex/(.+?).x86_64.rpm(.+?)', 'http://downloads.asperasoft.com/en/downloads/6')
+ConsoleDownload = findDownload('(.+?)/download/sw/console/(.+?).x86_64.rpm(.+?)', 'http://downloads.asperasoft.com/en/downloads/3')
+
+print EntSrvDownload
+print SharesDownload
+print FaspexDownload
+print ConsoleDownload
 
 exit()
