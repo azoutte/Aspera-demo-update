c44ecc0a2399d53bf5301a1f5402baa2eafc99dd
Andy Zoutte <andy@Andy-Zouttes-MacBook-Pro.local>
2016-10-17 12:44:28 -0600

    comments

--- license_scrape.py
+++ license_scrape.py
@@ -1,6 +1,22 @@
+<<<<<<< HEAD
 
 import urllib.request, re, time, os, string, sys
 from urllib.request import urlopen
+=======
+##### pip install lxml
+# pip install requests
+# http://docs.python-guide.org/en/latest/scenarios/scrape/
+#from lxml import html
+#import requests
+#page = requests.get('http://https://license.aspera.us/evals')
+#tree = html.fromstring(page.content)
+#print(page)
+#print(tree)
+#import urllib.request
+#page = urllib.request.urlopen('http://hiscore.runescape.com/index_lite.ws?player=zezima')
+#print(page.read())
+import urllib, re, time, os
+>>>>>>> parent of a3e95fc... annnn
 homeDir="/Users/andy"
 
 sock = urllib.request.urlopen('https://license.aspera.us/evals')
