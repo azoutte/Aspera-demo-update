924c55693d244d41966b89461c6fb6a170fd5ea0
Andy Zoutte <andy@Andy-Zouttes-MacBook-Pro.local>
2016-10-17 12:49:14 -0600

    Revert "annnn"
    This reverts commit a3e95fc8352019378e70a646feb34e37bceea192.

--- license_scrape.py
+++ license_scrape.py
@@ -1,31 +1,13 @@
-<<<<<<< HEAD
 
-import urllib.request, re, time, os, string, sys
-from urllib.request import urlopen
-=======
-##### pip install lxml
-# pip install requests
-# http://docs.python-guide.org/en/latest/scenarios/scrape/
-#from lxml import html
-#import requests
-#page = requests.get('http://https://license.aspera.us/evals')
-#tree = html.fromstring(page.content)
-#print(page)
-#print(tree)
-#import urllib.request
-#page = urllib.request.urlopen('http://hiscore.runescape.com/index_lite.ws?player=zezima')
-#print(page.read())
 import urllib, re, time, os
->>>>>>> parent of a3e95fc... annnn
 homeDir="/Users/andy"
 
-sock = urllib.request.urlopen('https://license.aspera.us/evals')
+sock = urllib.urlopen('https://license.aspera.us/evals')
 htmlSource = sock.read()
 sock.close()
 arrayOfLines = htmlSource.split()
-print(len(htmlSource))
-for line in arrayOfLines:
-    print(line)
+#for line in arrayOfLines:
+#    print(line)
 n = arrayOfLines.index(b'<strong>Username</strong>') + 2
 passWord = arrayOfLines[n].decode("utf-8")
 passWord = re.sub('<[^<]+?>', '', passWord)
@@ -40,7 +22,7 @@ year = time.strftime("%Y")
 if not os.path.exists(homeDir + '/licenses'):
     print("it doesn't exist")
     os.mkdir(homeDir + '/licenses', 0o700)
-urllib.request.urlretrieve("https://license.aspera.us/evals/" + year + "/" + month + ".zip", homeDir + "/licenses/licenses.zip")
+urllib.urlretrieve("https://license.aspera.us/evals/" + year + "/" + month + ".zip", homeDir + "/licenses/licenses.zip")
 unzipCommand = 'unzip -o ' + homeDir + '/licenses/licenses.zip -d ' + homeDir + '/licenses'
 os.system(unzipCommand)
 
