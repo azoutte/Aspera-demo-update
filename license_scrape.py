c1fe36b48485fcb8ce4dbf14215a92c0289f952b
Andy Zoutte <andy@Andy-Zouttes-MacBook-Pro.local>
2016-12-09 13:55:32 -0700

    added print options and cleaned up output

--- license_scrape.py
+++ license_scrape.py
@@ -1,17 +1,22 @@
 
-import urllib, re, time, os, string
-homeDir = "/Users/andy"
-licFiles = homeDir + "/licenses"
-downloads = homeDir + "/dloads"
+import urllib, re, time, os, string, glob
+homeDir = os.path.expanduser("~")
+licDir = homeDir + "/asperalicenses"
+downloads = homeDir + "/asperadownloads"
 
+# Make the downloads directory if it doesn't already exist
 if not os.path.exists(downloads):
     print("Downloads doesn't exist.  Creating.")
     os.mkdir(downloads, 0o700)
 
-def find_download(stringMatch, URL):
+# Function to find and download the proper package.
+# Inputs are the unique string to find the version and the URL.
+def find_download(stringMatch, URL, actionToTake):
+#   Load the file
     sock = urllib.urlopen(URL)
     htmlSource = sock.read()
     sock.close()
+#   Store that in a list and search for the pattern that was passed to the function.
     arrayOfLines = htmlSource.split()
     for line in arrayOfLines:
         line =  str(line)
@@ -19,55 +24,72 @@ def find_download(stringMatch, URL):
             line = line[7:len(line)-9]
             break
     fileName = os.path.basename(line)
-    print fileName
-    if not os.path.exists(downloads + '/' + fileName):
-        print("Downloading " + downloads + "/" + fileName + " from " + line)
-        line = string.replace(line, 'http://', "http://" + userName + ":" + passWord + "@")
+#   If the download file already exists, delete it if the file is incomplete.
+#   Download it if it does not esist useing the username and password discovered
+#   in the main program.
+    if actionToTake == "printcommand":
+        line = "  wget --user=" + userName + " --password=" + passWord + " " + line
+        print(line)
+        return
+    line = string.replace(line, "http://", "http://" + userName + ":" + passWord + "@")
+    if not os.path.exists(downloads + "/" + fileName):
+        print("   Downloading " + downloads + "/" + fileName)
         Stat = urllib.urlretrieve(line, downloads + "/" + fileName)
-        print("Status: " + str(Stat))
-        print("downloaded complete.")
-        # delete old ones
-        osCommand = "rpm -K " + downloads + "/" + fileName
+#        print("   Status: " + str(Stat))
+        # add functionality to delete old packages
+        osCommand = "rpm -K " + downloads + "/" + fileName + "> /dev/null"
         osReturn = os.system(osCommand)
-        print("RPM Status: " + str(osReturn))
+        print("   Download complete.  RPM validation exit code (0 is good): " + str(osReturn))
         if osReturn >= 1:
-            print("Downloaded file is corrupt ot incomplete.  Removing it.")
+            print("  Downloaded file is corrupt or incomplete.  Removing it.")
             os.remove(downloads + "/" + fileName)
     else:
-        print("file already downloaded")
+        print("   Skipping requested download of " + fileName + ".  It already exists")
     return line
 
-
-sock = urllib.urlopen('https://license.aspera.us/evals')
+# Pull the temporary username/password for downloading packages from the license website
+sock = urllib.urlopen("https://license.aspera.us/evals")
 htmlSource = sock.read()
 sock.close()
 arrayOfLines = htmlSource.split()
-n = arrayOfLines.index(b'<strong>Username</strong>') + 2
-userName = arrayOfLines[n].decode("utf-8")
-userName = re.sub('<[^<]+?>', '', userName)
-print("username=" + userName)
-n = arrayOfLines.index(b'<strong>Password</strong>') + 2
-passWord = arrayOfLines[n].decode("utf-8")
-passWord = re.sub('<[^<]+?>', '', passWord)
-print("password=" + passWord)
-
+n = arrayOfLines.index(b"<strong>Username</strong>") + 2
+userName = re.sub("<[^<]+?>", "", arrayOfLines[n].decode("utf-8"))
+n = arrayOfLines.index(b"<strong>Password</strong>") + 2
+passWord = re.sub("<[^<]+?>", "", arrayOfLines[n].decode("utf-8"))
+print("username=" + userName + "  password=" + passWord)
+print
 month = time.strftime("%m")
 year = time.strftime("%Y")
-if not os.path.exists(licFiles):
+
+# Make a directory for the licenses if one does not already exist.  If it already exists, delete the old contents.
+if not os.path.exists(licDir):
     print("License path doesn't exist.  Creating.")
-    os.mkdir(licFiles, 0o700)
-urllib.urlretrieve("https://license.aspera.us/evals/" + year + "/" + month + ".zip", homeDir + "/licenses/licenses.zip")
-unzipCommand = 'unzip -o ' + homeDir + '/licenses/licenses.zip -d ' + homeDir + '/licenses'
+    os.mkdir(licDir, 0o700)
+else:
+    map(os.remove, glob.glob(homeDir + "/licenses/*eval*license"))
+
+# Download the license file and unzip it.
+urllib.urlretrieve("https://license.aspera.us/evals/" + year + "/" + month + ".zip", licDir + "/licenses.zip")
+unzipCommand = "unzip -o " + licDir + "/licenses.zip -d " + licDir + "> /dev/null"
 os.system(unzipCommand)
+print("The current eval licenses have been downloaded to: " + licDir)
+print
 
-EntSrvDownload = find_download('(.+?)/download/sw/entsrv(.+?)-linux-64.rpm(.+?)', 'http://downloads.asperasoft.com/en/downloads/1')
-#SharesDownload = find_download('(.+?)/download/sw/shares/(.+?).x86_64.rpm(.+?)', 'http://downloads.asperasoft.com/en/downloads/34')
-#FaspexDownload = find_download('(.+?)/download/sw/faspex/(.+?).x86_64.rpm(.+?)', 'http://downloads.asperasoft.com/en/downloads/6')
-#ConsoleDownload = find_download('(.+?)/download/sw/console/(.+?).x86_64.rpm(.+?)', 'http://downloads.asperasoft.com/en/downloads/3')
-
-print EntSrvDownload
-#print SharesDownload
-#print FaspexDownload
-#print ConsoleDownload
+# Using the specific string for the pacckage, download the latest version
+print("Aspera Client - Linux")
+EntSrvDownload = find_download("(.+?)/download/sw/client(.+?)-linux-64.rpm(.+?)", "http://downloads.asperasoft.com/en/downloads/2", "printcommand")
+EntSrvDownload = find_download("(.+?)/download/sw/client(.+?)-linux-64.rpm(.+?)", "http://downloads.asperasoft.com/en/downloads/2", "download")
+print("Point to Point - Linux")
+EntSrvDownload = find_download("(.+?)/download/sw/p2p(.+?)-linux-64.rpm(.+?)", "http://downloads.asperasoft.com/en/downloads/7", "printcommand")
+print("Enterprise Server - Linux")
+EntSrvDownload = find_download("(.+?)/download/sw/entsrv(.+?)-linux-64.rpm(.+?)", "http://downloads.asperasoft.com/en/downloads/1", "printcommand")
+print("Shares - Linux")
+SharesDownload = find_download("(.+?)/download/sw/shares/(.+?).x86_64.rpm(.+?)", "http://downloads.asperasoft.com/en/downloads/34", "printcommand")
+print("Faspex - Linux")
+FaspexDownload = find_download("(.+?)/download/sw/faspex/(.+?).x86_64.rpm(.+?)", "http://downloads.asperasoft.com/en/downloads/6", "printcommand")
+FaspexDownload = find_download("(.+?)/download/sw/common/1.1(.+?).x86_64.rpm(.+?)", "http://downloads.asperasoft.com/en/downloads/6", "printcommand")
+print("Console - Linux")
+ConsoleDownload = find_download("(.+?)/download/sw/console/(.+?).x86_64.rpm(.+?)", "http://downloads.asperasoft.com/en/downloads/3", "printcommand")
+ConsoleDownload = find_download("(.+?)/download/sw/common/1.2(.+?).x86_64.rpm(.+?)", "http://downloads.asperasoft.com/en/downloads/3", "printcommand")
 
 exit()
