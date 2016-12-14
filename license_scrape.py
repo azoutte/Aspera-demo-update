9b2137498af3091e753ea5c3cd5188d8fde65819
Andy Zoutte <andy@Andy-Zouttes-MacBook-Pro.local>
2016-12-14 13:32:54 -0700

    added guide and some comments

--- license_scrape.py
+++ license_scrape.py
@@ -1,5 +1,7 @@
-
+#!/usr/bin/python
 import urllib, shutil, re, time, os, string, glob
+
+# CUSTOM: Edit these variables for your environement.  Especially the ssh key.
 homeDir = os.path.expanduser("~")
 licDir = homeDir + "/asperalicenses"
 dloadDir = homeDir + "/asperadownloads"
@@ -13,6 +15,9 @@ if not os.path.exists(dloadDir):
 # Function to find and download the proper package.
 # Inputs are the unique string to find the version and the URL.
 def download(stringMatch, URL, actionToTake):
+# Add some command strings to the passed variables
+    stringMatch= "(.+?)/download/sw/" + stringMatch 
+    URL =  "http://downloads.asperasoft.com/en/downloads/" + URL
 #   Load the file
     sock = urllib.urlopen(URL)
     htmlSource = sock.read()
@@ -31,23 +36,24 @@ def download(stringMatch, URL, actionToTake):
 #   in the main program.
     if actionToTake == "print":
         line = "  wget --user=" + userName + " --password=" + passWord + " " + line
-        print(line)
+        outFile.write(line + "\n")
         return
     line = string.replace(line, "http://", "http://" + userName + ":" + passWord + "@")
     if not os.path.exists(dloadDir + "/" + fileName):
-        print("   Downloading " + dloadDir + "/" + fileName + " to " + dloadDir)
+        print("Downloading " + dloadDir + "/" + fileName + " to " + dloadDir)
         Stat = urllib.urlretrieve(line, dloadDir + "/" + fileName)
-#        print("   Status: " + str(Stat))
+#        print("Status: " + str(Stat))
         # add functionality to delete old packages
         osCommand = "rpm -K " + dloadDir + "/" + fileName + "> /dev/null"
         osReturn = os.system(osCommand)
-        print("   Download complete.  RPM validation exit code (0 is good): " + str(osReturn))
+        print("Download complete.  RPM validation exit code: " + str(osReturn))
         if osReturn >= 1:
             print("  Downloaded file is corrupt or incomplete.  Removing it.")
             os.remove(dloadDir + "/" + fileName)
     else:
-        print("   Skipping requested download of " + fileName + ".  It already exists in " + dloadDir)
+        print("Skipping requested download of " + fileName + ".  It already exists in " + dloadDir)
     return line
+
 # This fuction builds the scp command from the passed parameters and either runs the command or just prints
 # is so that the user can run it manually.
 def license(license, server, action):
@@ -55,7 +61,7 @@ def license(license, server, action):
    if action == "push":
       print("Running " + osCommand)
       osReturn = os.system(osCommand)
-      print("   Scp complete.  Exit code (0 is good): " + str(osReturn))
+      print("   Scp complete.  Exit code: " + str(osReturn))
    else:
       print("You can manually run: " + command)
 
@@ -94,27 +100,45 @@ for licenseType in ["ConnectSrv", "Faspex-Users", "EntSrv-unlim", "Client", "Ent
       shutil.copy2(file, licDir + "/" + licenseType + str(count))
       count += 1
 
-# Using the specific string for the package, download the latest version
-print("Aspera Client - Linux")
-download("(.+?)/download/sw/client(.+?)-linux-64.rpm(.+?)", "http://downloads.asperasoft.com/en/downloads/2", "print")
-download("(.+?)/download/sw/client(.+?)-linux-64.rpm(.+?)", "http://downloads.asperasoft.com/en/downloads/2", "download")
-print("Point to Point - Linux")
-download("(.+?)/download/sw/p2p(.+?)-linux-64.rpm(.+?)", "http://downloads.asperasoft.com/en/downloads/7", "print")
-print("Enterprise Server - Linux")
-download("(.+?)/download/sw/entsrv(.+?)-linux-64.rpm(.+?)", "http://downloads.asperasoft.com/en/downloads/1", "print")
-print("Shares - Linux")
-download("(.+?)/download/sw/shares/(.+?).x86_64.rpm(.+?)", "http://downloads.asperasoft.com/en/downloads/34", "print")
-print("Faspex - Linux")
-download("(.+?)/download/sw/faspex/(.+?).x86_64.rpm(.+?)", "http://downloads.asperasoft.com/en/downloads/6", "print")
-download("(.+?)/download/sw/common/1.1(.+?).x86_64.rpm(.+?)", "http://downloads.asperasoft.com/en/downloads/6", "print")
-print("Console - Linux")
-download("(.+?)/download/sw/console/(.+?).x86_64.rpm(.+?)", "http://downloads.asperasoft.com/en/downloads/3", "print")
-download("(.+?)/download/sw/common/1.2(.+?).x86_64.rpm(.+?)", "http://downloads.asperasoft.com/en/downloads/3", "print")
+# Using the specific string for the package, download the latest version.  This section is optional.
+# You can add products here and set the last parameter to download and you will have an
+# automatic repository of the builds.  You can set the last parameter to print and it will simply
+# print the wget commands for you to manually use.
+print("Writing the list of wget commands to " + licDir + "/wget_commands")
 print
+# CUSTOM: Not requires, but you can add or remove from this list as needed.  But
+# you will have to visit the website for the exact formating as it is not consistent.
+outFile = open(licDir + "/wget_commands", "w")
+outFile.write("Aspera Client - Linux \n")
+download("client(.+?)-linux-64.rpm", "2", "print")
+outFile.write("Point to Point - Linux \n")
+download("p2p(.+?)-linux-64.rpm", "7", "print")
+outFile.write("Enterprise Server - Linux \n")
+download("entsrv(.+?)-linux-64.rpm", "1", "print")
+outFile.write("Shares - Linux \n")
+download("shares/(.+?).x86_64.rpm", "34", "print")
+outFile.write("Faspex - Linux \n")
+download("faspex/(.+?).x86_64.rpm", "6", "print")
+download("common/1.1(.+?).x86_64.rpm", "6", "print")
+outFile.write("Console - Linux \n")
+download("console/(.+?).x86_64.rpm", "3", "print")
+download("common/1.2(.+?).x86_64.rpm", "3", "print")
+outFile.write("Proxy - Linux \n")
+download("proxy/(.+?)-linux-64-release.rpm", "42", "print")
+outFile.write("Faspstream - Linux \n")
+download("faspstream/(.+?)-linux-64.rpm", "60", "print")
+outFile.write
+outFile.close()
 
-# This section prints or pushes out the license files.  If the last parameter of the function is print, then
+# CUSTOM: Remove this sample line if you don't want to maintain a GA release respository and
+# add extra lines if you want to download more GA builds
+download("client(.+?)-linux-64.rpm", "2", "download")
+
+# This section prints or pushes out the license files.  If the last parameter of the function to print, then
 # the scp line is printed so that it can be run manually.  If the last parameter is push, then the license
 # is pushed to the destination.      
+print
+# CUSTOM: Use this function to scp licenses out to your servers.
 license("ConnectSrv1", "miniserver1", "push")
 license("ConnectSrv2", "miniserver2", "push")
 license("ConnectSrv3", "minifaspex", "push")
